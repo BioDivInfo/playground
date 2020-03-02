8b51602fd74647ddaf63ce0995e49aed7a3ee480
Laura Rocha Prado <lauraprado@asu.edu>
2020-03-02 16:43:40 -0700

    adiciona playground

---
+++ site/gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    title: `BioDivInfo Playground`,
+    name: `Laura Rocha Prado`,
+    siteUrl: `https://gatsby-theme-document.netlify.com`,
+    description: `Ferramenta para colaboradores testarem o sistema de gerenciamento de conteúdo usado no Arquivo`,
+    social: [{
+      name: `github`,
+      url: `https://github.com/BioDivInfo/`
+    }],
+    sidebarConfig: {
+      forcedNavOrder: ["/comece", "/capitulo"],
+      ignoreIndex: true
+    }
+  },
+  plugins: [{ resolve: `gatsby-theme-document` }]
+};
